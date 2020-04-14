95bd5b5411324e44f09d50fef01d099ae913da02
Luna <francoguns16@gmail.com>
2020-04-13 23:24:42 -0500

    Updates

---
+++ precache-manifest.7ab39eb6290e31757b1303e7bbfa834d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fc3ef3a8b3d847bfef9426cc110b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f08eb60668749d6beed8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da8f9ad83e5287ba0ada",
+    "url": "/static/js/2.f67ad873.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f67ad873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08eb60668749d6beed8",
+    "url": "/static/js/main.9aa820cd.chunk.js"
+  },
+  {
+    "revision": "a235b1450ce30b0b0783",
+    "url": "/static/js/runtime-main.b84ad572.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
